93499c8987479469734970f17b7a7f676b9ef59b
Aditya Kumar <k.aditya00@gmail.com>
2020-12-15 09:39:28 +0530

    feat(db): UserModel

---
+++ src/models/User.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+export const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  mobile: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+});
+
+export const UserModel = mongoose.model("User", UserSchema);
